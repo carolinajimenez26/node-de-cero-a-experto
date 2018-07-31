07ec09562220dab7bd435c45b937b73e4259c0dc
Carolina <carolina.jimenez.g@gmail.com>
2018-07-31 15:21:45 -0500

    Add some color :)

--- yargs/config/app.js
+++ yargs/config/app.js
@@ -1,14 +1,15 @@
-let {argv} = require('./yargs');
+const {argv} = require('./yargs');
+const colors = require('colors/safe');
 
 let command = argv._[0];
 
 switch(command) {
     case 'sayHello':
-        console.log(`Hello ${argv.name}`);
+        console.log(colors.rainbow(`Hello ${argv.name}`));
     break;
 
     case 'sayGoodBye':
-    console.log(`Good bye ${argv.name}`);
+    console.log(colors.cyan(`Good bye ${argv.name}`));
     break;
 
     default:
